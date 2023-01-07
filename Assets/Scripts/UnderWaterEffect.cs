b452337cdab4ae05fc1a172ce1218b754fe930d0
Abigail Evans <cdn_abigail@outlook.com>
2023-01-07 19:14:30 -0400

    (Added) Start of Screen Noise
    - Got the screen noise shader to work onscreen,
    
     not very pretty at the moment. But functioning

---
+++ Assets/Scripts/UnderWaterEffect.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class UnderWaterEffect : MonoBehaviour
+
+{
+    public Material _mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, _mat);
+    }
+}
