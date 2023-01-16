083dfb631dc64f57a51756ad3e332420d85bef10
Kyle Dixon <90637263+KyleDixonVO@users.noreply.github.com>
2023-01-16 13:52:07 -0400

    [ADDED] Started repair mechanic

---
+++ Assets/Scripts/RepairObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairObject : MonoBehaviour
+{
+    [SerializeField] Vector3 repairDestination;
+    [SerializeField] float repairTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Repair()
+    {
+
+    }
+}
