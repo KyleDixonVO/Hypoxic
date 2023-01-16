e86d0a6f77768f9eb4d979c169f7dac9edf53b07
Tobias Gagnon <90637945+TheShadowRaptor@users.noreply.github.com>
2023-01-16 15:01:53 -0400

    [ADDED] Namespaces
    Added namespaces to the weapons

--- Assets/Scripts/Weapons/HarpoonGun.cs
+++ Assets/Scripts/Weapons/HarpoonGun.cs
@@ -2,60 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HarpoonGun : Weapon
+namespace UnderwaterHorror
 {
-    // We might want to put items like this on a diffrent rendering plane / canvas to prevent
-    // clipping with the enviroment - Edmund
-
-    // Start is called before the first frame update
-    void Start()
+    public class HarpoonGun : Weapon
     {
-        // set weapon stats
+        // We might want to put items like this on a diffrent rendering plane / canvas to prevent
+        // clipping with the enviroment - Edmund
 
-        // set bools
-        canShoot = true;
-        isEquiped = true;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // RELOAD COUTER
-        if (reloadProgress <= 0)
+        // Start is called before the first frame update
+        void Start()
         {
-            reloadProgress = 0;
+            // set weapon stats
+
+            // set bools
             canShoot = true;
-        }
-        else if (reloadProgress > 0 && isEquiped)
-        {
-            reloadProgress -= Time.deltaTime;
-        }
-
-        // SHOOTING
-        if (Input.GetKeyDown(KeyCode.Mouse0) && currentAmmo > 0 && canShoot && isEquiped)
-        {
-            ShootWeapon();
-        }
-        else if (Input.GetKeyDown(KeyCode.Mouse0) && currentAmmo <= 0 && isEquiped)
-        {
-            // no ammo sound
-        }
-
-        //TEMP
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
             isEquiped = true;
-            gameObject.GetComponent<Renderer>().enabled = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2) | Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            isEquiped = false;
-            gameObject.GetComponent<Renderer>().enabled = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.Q))
+        // Update is called once per frame
+        void Update()
         {
-            //EquipUnequip();
+            // RELOAD COUTER
+            if (reloadProgress <= 0)
+            {
+                reloadProgress = 0;
+                canShoot = true;
+            }
+            else if (reloadProgress > 0 && isEquiped)
+            {
+                reloadProgress -= Time.deltaTime;
+            }
+
+            // SHOOTING
+            if (Input.GetKeyDown(KeyCode.Mouse0) && currentAmmo > 0 && canShoot && isEquiped)
+            {
+                ShootWeapon();
+            }
+            else if (Input.GetKeyDown(KeyCode.Mouse0) && currentAmmo <= 0 && isEquiped)
+            {
+                // no ammo sound
+            }
+
+            //TEMP
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                isEquiped = true;
+                gameObject.GetComponent<Renderer>().enabled = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2) | Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                isEquiped = false;
+                gameObject.GetComponent<Renderer>().enabled = false;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                //EquipUnequip();
+            }
         }
     }
+
 }
