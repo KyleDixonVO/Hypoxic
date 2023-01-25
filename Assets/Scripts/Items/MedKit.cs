c3d25dc704f856f5f69bdf7a94cec4ad3ffbb4df
Edmund Wohlmuth <74564582+EdmundWohlmuth@users.noreply.github.com>
2023-01-25 10:40:14 -0400

    Worked on items

--- Assets/Scripts/Items/MedKit.cs
+++ Assets/Scripts/Items/MedKit.cs
@@ -5,22 +5,44 @@ using UnityEngine;
 public class MedKit : Item
 {
     // Start is called before the first frame update
+    [SerializeField] float healthGain = 50f;
+
     void Start()
     {
-        
+        usageTime = 2;
+        usageProgress = usageTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Alpha0))
+        // if (GameManager.gameManager.gameState != GameManager.gameStates.gameplay) return;
+        if (Input.GetKeyDown(KeyCode.Alpha3))
         {
+            Equip();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Unequip();
+        }
 
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            beingUsed = true;
+            TimeToEffect();
+            Debug.Log("click");
         }
     }
 
     override protected void ApplyEffect()
     {
         // run code
+        PlayerStats.playerStats.playerHealth += healthGain;
+        Debug.LogWarning("works");
+
+        if (PlayerStats.playerStats.playerHealth > PlayerStats.playerStats.maxPlayerHealth)
+        {
+            PlayerStats.playerStats.playerHealth = PlayerStats.playerStats.maxPlayerHealth;
+        }
     }
 }
