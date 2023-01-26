f74e70d18e04eecf4aef98c0705d0495b2659329
Edmund Wohlmuth <74564582+EdmundWohlmuth@users.noreply.github.com>
2023-01-26 11:08:28 -0400

    finally fixed doors

--- Assets/Scripts/Airlock/AirlockNoLoadTrigger.cs
+++ Assets/Scripts/Airlock/AirlockNoLoadTrigger.cs
@@ -4,12 +4,43 @@ using UnityEngine;
 
 public class AirlockNoLoadTrigger : MonoBehaviour
 {
-    public Airlock airlockToOpen;
-    public Airlock airlockToClose;
+    public Airlock exteriorAirlock;
+    public Airlock interiorAirlock;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) // why did it take me so long figure this out... - Edmund
     {
-        airlockToOpen.OpenDoor();
-        airlockToClose.CloseDoor(0f);
+        if (other.tag == "Player") // detects which door is open and which is closed and opens/closes accordingly
+                                   // assumes that one is closed and one is open, God knows what happens if that isn't the case
+                                   // just don't let it happen. - Edmund
+        {
+            //Debug.Log("player present");
+            if (exteriorAirlock.isOpening)
+            {
+                exteriorAirlock.CloseDoor(0f);
+                interiorAirlock.OpenDoor();
+            }
+            else if (interiorAirlock.isOpening)
+            {
+                interiorAirlock.CloseDoor(0f);
+                exteriorAirlock.OpenDoor();
+            }
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (exteriorAirlock.isOpening)
+            {
+                exteriorAirlock.CloseDoor(2f);
+            }
+            else if (interiorAirlock.isOpening)
+            {
+                interiorAirlock.CloseDoor(2f);
+            }
+        }
+
     }
 }
