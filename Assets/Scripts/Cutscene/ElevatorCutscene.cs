44839e9ae984ebb367e1fab51eeadd83f5d394ca
Edmund Wohlmuth <74564582+EdmundWohlmuth@users.noreply.github.com>
2023-03-29 12:21:58 -0300

    worked on outro cutscene

--- Assets/Scripts/Cutscene/ElevatorCutscene.cs
+++ Assets/Scripts/Cutscene/ElevatorCutscene.cs
@@ -7,7 +7,6 @@ namespace UnderwaterHorror
     public class ElevatorCutscene : MonoBehaviour
     {
         Animator animator;
-        [SerializeField] Animation introAnim;
 
         // Start is called before the first frame update
         void Start()
@@ -23,11 +22,20 @@ namespace UnderwaterHorror
 
         void PauseCheck()
         {
-            if (GameManager.gameManager.gameState == GameManager.gameStates.paused)
+            if (GameManager.gameManager.gameState == GameManager.gameStates.paused || UI_Manager.ui_Manager.PDAOpen())
             {
                 animator.speed = 0;
             }
-            else if (GameManager.gameManager.gameState != GameManager.gameStates.paused && animator.speed != 1) animator.speed = 1;
+            else if (GameManager.gameManager.gameState != GameManager.gameStates.paused || UI_Manager.ui_Manager.PDAOpen() == false && animator.speed != 1) animator.speed = 1;
+        }
+
+        public void GoToCutScene()
+        {
+            GameManager.gameManager.gameState = GameManager.gameStates.cutscene;
+        }
+        public void GoToGameplay()
+        {
+            GameManager.gameManager.gameState = GameManager.gameStates.gameplay;
         }
     }
 }
