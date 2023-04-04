1e5d7c1fa07f911730c3e8b058370863f97346e6
Tobias Gagnon <90637945+TheShadowRaptor@users.noreply.github.com>
2023-04-04 12:46:50 -0300

    [UPDATED] ReactorSounds

--- Assets/Scripts/Items/ObjectiveRelated/ReactorButton.cs
+++ Assets/Scripts/Items/ObjectiveRelated/ReactorButton.cs
@@ -8,16 +8,29 @@ namespace UnderwaterHorror
     {
         [SerializeField] string tooltip;
         [SerializeField] bool reactorOn;
+        [SerializeField] bool reactorStartOn;
         [SerializeField] AudioSource reactorSoundHolder;
         // Start is called before the first frame update
         void Start()
         {
+            reactorStartOn = false;
             reactorOn = false;
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (GameManager.gameManager.gameState != GameManager.gameStates.gameplay)
+            {
+                AudioManager.audioManager.PauseSound(reactorSoundHolder);
+                return;
+            }
+
+            if (reactorStartOn == true)
+            {
+                if (reactorSoundHolder.isPlaying == false) reactorOn = true;
+            }
+
             if (reactorOn)
             {
                 AudioManager.audioManager.PlaySound(reactorSoundHolder, AudioManager.audioManager.reactorOn);
@@ -39,7 +52,7 @@ namespace UnderwaterHorror
         {
             Objective_Manager.objective_Manager.UpdateObjectiveCompletion((int)Objective_Manager.Objectives.activateReactor);
             if (reactorOn == false) AudioManager.audioManager.PlaySound(reactorSoundHolder, AudioManager.audioManager.reactorStartUp);
-            if (reactorSoundHolder.isPlaying == false) reactorOn = true;
+            reactorStartOn = true;
         }
     }
 }
